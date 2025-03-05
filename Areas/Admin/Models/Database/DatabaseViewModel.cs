82becf71a1b4a8fe639a2a953706ba1d9d3ff43b
potuta <cyrilbautista2@gmail.com>
2025-03-05 17:21:54 +0800

    Implemented individual backup, restore, delete database

--- Areas/Admin/Models/Database/DatabaseViewModel.cs
+++ Areas/Admin/Models/Database/DatabaseViewModel.cs
@@ -6,6 +6,10 @@ namespace LittleArkFoundation.Areas.Admin.Models.Database
 {
     public class DatabaseViewModel
     {
+        public string? DatabaseName { get; set; }
+        public string? CurrentConnectionString { get; set; }
+        public string? DefaultConnectionString { get; set; }
         public Dictionary<string, string>? Databases { get; set; }
+        public List<string>? DatabaseBackupFiles { get; set; }
     }
 }
