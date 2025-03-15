96a4bd30e8304a78ac4a33fa8d9efb0ece2a6114
potuta <cyrilbautista2@gmail.com>
2025-03-15 23:01:29 +0800

    Added MonthlyExpenses and Utilities models

---
+++ Areas/Admin/Models/MonthlyExpenses/MonthlyExpensesModel.cs
@@ -0,0 +1,18 @@
+﻿namespace LittleArkFoundation.Areas.Admin.Models.MonthlyExpenses
+{
+    public class MonthlyExpensesModel
+    {
+        public int ExpenseID { get; set; }
+        public int PatientID { get; set; }
+        public decimal HouseAndLot { get; set; }
+        public decimal FoodAndWater { get; set; }
+        public decimal Education { get; set; }
+        public decimal Clothing { get; set; }
+        public decimal Communication { get; set; }
+        public decimal HouseHelp { get; set; }
+        public decimal MedicalExpenses { get; set; }
+        public decimal Transportation { get; set; }
+        public decimal Others { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ Areas/Admin/Models/MonthlyExpenses/UtilitiesModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LittleArkFoundation.Areas.Admin.Models.MonthlyExpenses
+{
+    public class UtilitiesModel
+    {
+        public int UtilityID { get; set; }
+        public int PatientID { get; set; }
+        public string LightSource { get; set; }
+        public decimal LightSourceAmount { get; set; }
+        public string FuelSource { get; set; }
+        public decimal FuelSourceAmount { get; set; }
+        public string WaterSource { get; set; }
+    }
+}
