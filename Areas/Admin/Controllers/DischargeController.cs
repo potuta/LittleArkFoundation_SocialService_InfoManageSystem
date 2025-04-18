4840dbba71d066717038ecc569d5fe4e1d4d90e2
potuta <cyrilbautista2@gmail.com>
2025-04-18 23:10:04 +0800

    Implemented OPD and Discharge views and controllers

---
+++ Areas/Admin/Controllers/DischargeController.cs
@@ -0,0 +1,15 @@
+﻿using LittleArkFoundation.Authorize;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleArkFoundation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [HasPermission("ManageDischarge")]
+    public class DischargeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/OPDController.cs
@@ -0,0 +1,15 @@
+﻿using LittleArkFoundation.Authorize;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LittleArkFoundation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [HasPermission("ManageOPD")]
+    public class OPDController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
