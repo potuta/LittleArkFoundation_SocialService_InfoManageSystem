029bdf7944a6fa36e2dc1224f4406d9c8d077ab4
potuta <cyrilbautista2@gmail.com>
2025-05-25 13:29:33 +0800

    Implemented Dashboard Statistics

---
+++ Areas/Admin/Models/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LittleArkFoundation.Areas.Admin.Models.Assessments;
+using LittleArkFoundation.Areas.Admin.Models.Discharges;
+
+namespace LittleArkFoundation.Areas.Admin.Models.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public List<AssessmentsModel> DailyAssessments { get; set; } = new List<AssessmentsModel>();
+        public List<AssessmentsModel> MonthlyAssessments { get; set; } = new List<AssessmentsModel>();
+        public List<AssessmentsModel> YearlyAssessments { get; set; } = new List<AssessmentsModel>();
+        public List<DischargesModel> DailyDischarges { get; set; } = new List<DischargesModel>();
+        public List<DischargesModel> MonthlyDischarges { get; set; } = new List<DischargesModel>();
+        public List<DischargesModel> YearlyDischarges { get; set; } = new List<DischargesModel>();
+    }
+}
