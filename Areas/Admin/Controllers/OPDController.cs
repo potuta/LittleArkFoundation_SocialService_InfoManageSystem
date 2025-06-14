acc9c47db0c3fc27b79f0dabc46ecab8ba71d37c
potuta <cyrilbautista2@gmail.com>
2025-06-14 10:11:01 +0800

    Implemented delete OPD

--- Areas/Admin/Controllers/OPDController.cs
+++ Areas/Admin/Controllers/OPDController.cs
@@ -132,5 +132,38 @@ namespace LittleArkFoundation.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        [HasPermission("DeleteOPD")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                string connectionString = _connectionService.GetCurrentConnectionString();
+                await using var context = new ApplicationDbContext(connectionString);
+                var opd = await context.OPD.FindAsync(id);
+                if (opd == null)
+                {
+                    TempData["ErrorMessage"] = "OPD not found.";
+                    return RedirectToAction("Index");
+                }
+                context.OPD.Remove(opd);
+                await context.SaveChangesAsync();
+                TempData["SuccessMessage"] = $"Successfully deleted OPD Id: {id}";
+                LoggingService.LogInformation($"OPD Patient deleted successful. Deleted OPD Id: {id}. Deleted by UserID: {User.FindFirst(ClaimTypes.NameIdentifier).Value}, DateTime: {DateTime.Now}");
+                return RedirectToAction("Index");
+            }
+            catch (SqlException se)
+            {
+                TempData["ErrorMessage"] = "SQL Error: " + se.Message;
+                LoggingService.LogError("SQL Error: " + se.Message);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Error: " + ex.Message;
+                LoggingService.LogError("Error: " + ex.Message);
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
