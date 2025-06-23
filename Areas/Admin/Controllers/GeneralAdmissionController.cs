3bf1ea23902ff2e0039310a5a19c444e97fa55a3
potuta <cyrilbautista2@gmail.com>
2025-06-23 18:53:52 +0800

    implemented GeneralAdmission Edit in controller

--- Areas/Admin/Controllers/GeneralAdmissionController.cs
+++ Areas/Admin/Controllers/GeneralAdmissionController.cs
@@ -1,9 +1,12 @@
-﻿using LittleArkFoundation.Areas.Admin.Models.GeneralAdmission;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using LittleArkFoundation.Areas.Admin.Models.GeneralAdmission;
 using LittleArkFoundation.Areas.Admin.Models.OPD;
 using LittleArkFoundation.Authorize;
 using LittleArkFoundation.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace LittleArkFoundation.Areas.Admin.Controllers
 {
@@ -34,6 +37,51 @@ namespace LittleArkFoundation.Areas.Admin.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            string connectionString = _connectionService.GetCurrentConnectionString();
+            await using var context = new ApplicationDbContext(connectionString);
+
+            var generalAdmission = await context.GeneralAdmission.FindAsync(id);
+
+            var viewModel = new GeneralAdmissionViewModel
+            {
+                GeneralAdmission = generalAdmission,
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(GeneralAdmissionViewModel viewModel)
+        {
+            try
+            {
+                string connectionString = _connectionService.GetCurrentConnectionString();
+                await using var context = new ApplicationDbContext(connectionString);
+
+                context.GeneralAdmission.Update(viewModel.GeneralAdmission);
+                await context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = $"Successfully edited General Admission Id: {viewModel.GeneralAdmission.Id}";
+                LoggingService.LogInformation($"General Admission Patient edit successful. Edited Id: {viewModel.GeneralAdmission.Id}. Edited by UserID: {User.FindFirst(ClaimTypes.NameIdentifier).Value}, DateTime: {DateTime.Now}");
+                return RedirectToAction("Index");
+            }
+            catch (SqlException se)
+            {
+                TempData["ErrorMessage"] = "SQL Error: " + se.Message;
+                LoggingService.LogError("SQL Error: " + se.Message);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Error: " + ex.Message;
+                LoggingService.LogError("Error: " + ex.Message);
+                return RedirectToAction("Index");
+            }
         }
     }
 }
