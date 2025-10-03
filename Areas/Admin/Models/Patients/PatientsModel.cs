753f0161c4da92e63d143060a0ec93cc63269a8e
potuta <cyrilbautista2@gmail.com>
2025-10-03 22:10:57 +0800

    Added more N/A and 0 fields

--- Areas/Admin/Models/Patients/PatientsModel.cs
+++ Areas/Admin/Models/Patients/PatientsModel.cs
@@ -6,7 +6,7 @@
         public string PatientType { get; set; } = "N/A";
         public bool IsActive { get; set; } = true;
         public string FirstName { get; set; }
-        public string MiddleName { get; set; }
+        public string MiddleName { get; set; } = "N/A";
         public string LastName { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string Sex { get; set; }
